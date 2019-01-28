0b83efefc69fd5756865242668a7f4213dceafd1
LucaGuerrazzi <45490013+LucaGuerrazzi@users.noreply.github.com>
2019-01-28 09:34:25 +0100

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,35 @@
+library(data.table)
+library(stringr)
+library(lubridate)
+
+plot1 <- function () {
+        #download the zip file if not already dowloaded
+        file_URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        if (!file.exists(".\\exdata_data_household_power_consumption.zip")) {
+                download.file(file_URL,".\\exdata_data_household_power_consumption.zip")
+        }
+        #unzip the zip archive if not already unzipped
+        if (!file.exists(".\\household_power_consumption.txt")) {
+                unzip(".\\exdata_data_household_power_consumption.zip")
+        }
+        
+        df <- fread(".\\household_power_consumption.txt", colClasses = c(Global_active_power = "num"))
+        
+        #select the only required records of the dataset
+        df <- subset(df,df$Date == "1/2/2007" | df$Date == "2/2/2007")
+        
+        #change the columns types to make elaboratin possible
+        df$Global_active_power <- as.numeric(df$Global_active_power)
+        df$Global_reactive_power <- as.numeric(df$Global_reactive_power)
+        df$Sub_metering_1 <- as.numeric(df$Sub_metering_1)
+        df$Sub_metering_2 <- as.numeric(df$Sub_metering_2)
+        df$Sub_metering_3 <- as.numeric(df$Sub_metering_3)
+        df$Voltage <- as.numeric(df$Voltage)
+        df$DateTime <- paste(df$Date,df$Time,sep = " ")
+        df$DateTime <- as.POSIXct(df$DateTime,format="%d/%m/%Y %H:%M:%OS")
+        
+        #create the plot
+        png(".\\plot1.png")
+        hist(df$Global_active_power, xlab = "Global Active Power (kilowatts)", main = "Global Active Power", col = "red")
+        dev.off()
+}
